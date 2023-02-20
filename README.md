2f1980a65b97e2edcc0bba501405bc000a1288ef
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-20 11:09:49 -0600

    Optimize minesweeper.py, update README.md with text and image, and add LICENSE.md to the Minesweeper project  This commit includes several changes to the Minesweeper project: * minesweeper.py has been optimized to improve performance * README.md has been updated with additional text and an image.png to provide a better user experience * A LICENSE.md file has been added to the Minesweeper project to clarify the licensing terms of the software.

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Minesweeper
+# Minesweeper Game
+
+![Screenshot of Minesweeper game being played](https://github.com/AlliysonBonner/Minesweeper/blob/main/image.png?raw=true)
+
+This is a console-based implementation of the classic Minesweeper game written in Python. The game is played on a two-dimensional grid, where some of the cells contain hidden bombs. The objective is to clear the board without detonating any bombs.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Gameplay](#gameplay)
+- [Features](#features)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+To install the game, you'll need to have Python 3 installed on your machine. You can download Python from the official website: https://www.python.org/downloads/.
+
+Once you have Python installed, download the `minesweeper.py` file from this repository and save it to your computer.
+
+## Usage
+To start the game, open a terminal window and navigate to the directory where you saved the `minesweeper.py` file. Then, run the following command:
+
+`python minesweeper.py`
+
+The game will start, and you'll be prompted to enter the size of the board and the number of bombs to be placed on the board.
+
+## Gameplay
+The game is played by entering coordinates of the cell you want to reveal or flag. To reveal a cell, enter its row and column numbers separated by a comma. To flag a cell, enter "F" followed by the row and column numbers separated by a comma.
+
+For example, to reveal the cell in the third row and fifth column, enter `3,5`. To flag the same cell, enter `F3,5`.
+
+If you reveal a cell containing a bomb, the game is over. If you reveal all the cells that don't contain bombs, you win!
+
+## Features
+- Customizable board size and number of bombs.
+- Flagging of cells to help keep track of potential bomb locations.
+- Recursive revealing of empty cells to speed up gameplay.
+- Colorful console output for better visualization of the board.
+
+## Contributing
+Contributions are welcome! If you find a bug or have a suggestion for an improvement, please open an issue or submit a pull request.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/Minesweeper/LICENSE.md) file for details.
+
+## Author
+* Name: Alliyson Bonner
+* Github: https://github.com/alliysonbonner
+* LinkedIn: https://www.linkedin.com/in/alliyson
+* Email: alliyson.bonner@gmail.com
--- minesweeper.py
+++ minesweeper.py
@@ -2,207 +2,256 @@ import random
 import re
 
 
-# lets create a board object to represent the mindsweeper game
-# this is so that we can just say "create a new board object", or
-# "dig here", or "render this game for this object"
 class Board:
+    """
+    The Board class models a Minesweeper game board.
 
+    Attributes:
+        dim_size (int): The size of the board.
+        num_bombs (int): The number of bombs on the board.
+        board (list): A 2D list representing the board.
+        dug (set): A set of tuples representing the dug cells.
+        flags (set): A set of tuples representing the flagged cells.
+
+    Methods:
+        place_bombs(): Randomly plants bombs on the board.
+        assign_values_to_board(): Assigns numerical values to the board based on neighboring bombs.
+        get_num_neighboring_bombs(row, col): Returns the number of neighboring bombs.
+        dig(row, col): Recursively digs cells until a bomb is found or cells adjacent to bombs are reached.
+        flag(row, col): Adds or removes a flag from the specified cell.
+        __str__(): Returns a string representation of the board.
+    """
     def __init__(self, dim_size, num_bombs):
-        # let's keep track of these parameters. they'll be helpful later
+        """Initializes the Board object.
+
+        Args:
+            dim_size (int): The size of the board.
+            num_bombs (int): The number of bombs on the board.
+        """
         self.dim_size = dim_size
         self.num_bombs = num_bombs
 
-        # let's create the board
-        # helper function!
-        self.board = self.make_new_board()  # plant the bombs
-        self.assign_values_to_board()
+        # Create an empty board
+        self.board = [[' ' for _ in range(self.dim_size)] for _ in range(self.dim_size)]
 
-        # initialize a set to keep track of which locations we've uncovered
-        # we'll save (row,col) tuples into this set
-        self.dug = set()  #if we dig at 0, 0, then self.dug = {(0,0)}
+        # Place bombs on the board
+        self.place_bombs()
 
-    def make_new_board(self):
-        # construct a new board based on the dim size and the num bombs
-        # we should construct the list of list here (or whatever representation you
-        # prefer, but since we have a 2-D board, list of lists is most natural)
+        # Keep track of cells that have been dug
+        self.dug = set()
 
-        # generate a new board
-        board = [[None for _ in range(self.dim_size)]
-                 for _ in range(self.dim_size)]
-        # this creates an array like this:
-        # [[None, None, ..., None].
-        # [None, None, ..., None],
-        # [...                  ],
-        # [None, None, ..., None,]]
-        # we can see how this represents a board!
+        # Keep track of cells that have flags
+        self.flags = set()
 
-        # plant the bombs
+    def place_bombs(self):
+        """
+        Randomly plants bombs on the board.
+
+        Returns:
+            None
+        """
+        # Initialize the number of bombs that have been planted
         bombs_planted = 0
+
+        # Keep randomly planting bombs until the specified number of bombs is reached
         while bombs_planted < self.num_bombs:
-            loc = random.randint(
-                0, self.dim_size**2 -
-                1)  #return a random integer N since that a <= N <= b
-            row = loc // self.dim_size
-            col = loc % self.dim_size
+            # Generate random coordinates
+            row, col = random.randint(0, self.dim_size - 1), random.randint(0, self.dim_size - 1)
 
-            if board[row][col] == '*':
-                #this means we've actually planted a bomb there already so keep going
-                continue
+            # If the current cell does not have a bomb already
+            if self.board[row][col] == ' ':
+                # Place a bomb in the current cell
+                self.board[row][col], bombs_planted = '*', bombs_planted + 1
 
-            board[row][col] = '*'  #plant the bomb
-            bombs_planted += 1
-
-        return board
 
     def assign_values_to_board(self):
-        # now that we have the bombs planted, let's assign a number 0-8 for all the empty spaces, which
-        # represents how many neighboring bombs there are. we can precompute these and it'll save us some
-        # effort checking what's around the board later on :)
-        for r in range(self.dim_size):
-            for c in range(self.dim_size):
-                if self.board[r][c] == '*':
-                    # if this is already a bomb, we don't want to calculate anything
-                    continue
-                self.board[r][c] = self.get_num_neighboring_bombs(r, c)
+        """
+        Assigns numerical values to the board based on neighboring bombs.
+        """
+        for r in range(self.dim_size):  # Loop over rows
+            for c in range(self.dim_size):  # Loop over columns
+                if self.board[r][c] == '*':  # If the current position contains a bomb
+                    continue  # Skip assigning a value to this position
+                # Assign the value of number of bombs in the neighboring cells to the current position
+                self.board[r][c] = str(self.get_num_neighboring_bombs(r, c))
 
     def get_num_neighboring_bombs(self, row, col):
-        # let's iterate through each of the neighboring positions and sum number of bombs
-        # top left: (row-1, col-1)
-        # top middle: (row-1, col)
-        # top right: (row-1, col+1)
-        # left: (row, col-1)
-        # right: (row, col+1)
-        # bottom left: (row+1, col-1)
-        # bottom middle: (row+1, col)
-        # bottom right: (row+1, col+1)
+        """
+        Returns the number of neighboring bombs.
 
-        # make sure to not go out of bounds!
+        Args:
+            row (int): The row index of the cell.
+            col (int): The column index of the cell.
 
-        num_neighboring_bombs = 0
+        Returns:
+            int: The number of neighboring bombs.
+        """
+        count = 0
+        # Loop through the neighboring cells
         for r in range(max(0, row - 1), min(self.dim_size - 1, row + 1) + 1):
-            for c in range(max(0, col - 1),
-                           min(self.dim_size - 1, col + 1) + 1):
-                if r == row and c == col:
-                    # our original location, don't check
-                    continue
-                if self.board[r][c] == '*':
-                    num_neighboring_bombs += 1
-
-        return num_neighboring_bombs
+            for c in range(max(0, col - 1), min(self.dim_size - 1, col + 1) + 1):
+                # If the neighboring cell has a bomb, increment the count
+                count += self.board[r][c] == '*'
+        return count
 
     def dig(self, row, col):
-        # dig at that location!
-        # return True if successful dig, False if bomb dug
+        """
+        Recursively digs cells until a bomb is found or cells adjacent to bombs are reached.
 
-        # a few scenarios:
-        # hit a bomb -> game over
-        # dig at location with neighboring bombs -> finsh dig
-        # dig at location with no neighboring bombs -> recursively dig neighbors!
+        Args:
+            row (int): The row index of the cell.
+            col (int): The column index of the cell.
 
-        self.dug.add((row, col))  # keep track that we dug here
+        Returns:
+            bool: True if the cell is safe, False if a bomb is found.
+        """
+        # Check if the cell has a flag
+        if (row, col) in self.flags:
+            # Ask the user if they really want to dig in a spot marked with a flag
+            confirm = input("This spot is marked with a flag. Are you sure you want to dig here? (y/n): ")
+            if confirm.lower() == 'n':
+                return True
+            elif confirm.lower() == 'y':
+                # Toggle the flag and dig in the cell
+                self.flag(row, col)
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
+                return True
 
+        # If the cell contains a bomb, mark it with an asterisk and return False
         if self.board[row][col] == '*':
+            self.dug.add((row, col))
+            self.board[row][col] = '*'  # Mark the cell with an asterisk
             return False
-        elif self.board[row][col] > 0:
-            return True
 
-        # self.board[row][col] == 0
-        for r in range(max(0, row - 1), min(self.dim_size - 1, row + 1) + 1):
-            for c in range(max(0, col - 1),
-                           min(self.dim_size - 1, col + 1) + 1):
-                if (r, c) in self.dug:
-                    continue  # don't dig where you've already dug
-                self.dig(r, c)
+        # Mark the cell as dug
+        self.dug.add((row, col))
 
-        # if our initial dig didn't hit a bomb, we *shouldn't* hit a bomb here
+        # If the cell is adjacent to 0 bombs, dig its neighbors recursively
+        if self.board[row][col] == '0':
+            for r in range(max(0, row - 1), min(self.dim_size - 1, row + 1) + 1):
+                for c in range(max(0, col - 1), min(self.dim_size - 1, col + 1) + 1):
+                    if (r, c) not in self.dug:
+                        self.dig(r, c)
+
         return True
 
+
+
+    def flag(self, row, col):
+        """
+        Toggles the flag in the specified cell.
+
+        Args:
+            row (int): The row index of the cell.
+            col (int): The column index of the cell.
+        """
+        if (row, col) not in self.dug:
+            if (row, col) not in self.flags:
+                # Add a flag to the cell
+                self.flags.add((row, col))
+                # If the cell doesn't contain a bomb, mark it with an 'F'
+                if self.board[row][col] != '*':
+                    self.board[row][col] = 'F'
+            else:
+                # Remove the flag from the cell
+                self.flags.remove((row, col))
+                # If the cell doesn't contain a bomb, mark it with a blank space
+                if self.board[row][col] != '*':
+                    self.board[row][col] = ' '
+
+
+
     def __str__(self):
-        # this is a magic function where if you call on this object,
-        # it'll print out what this function returns!
-        # return a string that shows the board to the player
+        """
+        Returns a string representation of the board.
 
-        # first let's create a new array that represents what the user would see
-        visible_board = [[None for _ in range(self.dim_size)]
-                         for _ in range(self.dim_size)]
-        for row in range(self.dim_size):
-            for col in range(self.dim_size):
-                if (row, col) in self.dug:
-                    visible_board[row][col] = str(self.board[row][col])
+        Returns:
+            str: A string representation of the board.
+        """
+        # Create the header row with the column indices
+        header = '   '
+        for i in range(self.dim_size):
+            header += f' {i}'
+
+        # Start the string with the header
+        s = f'{header}\n'
+
+        # Add the rows of the board
+        for i in range(self.dim_size):
+            row = f'{i} |'  # Start the row with the row index
+            for j in range(self.dim_size):
+                if (i, j) in self.dug:
+                    if self.board[i][j] == '*':
+                        row += ' *'
+                    else:
+                        row += f' {self.board[i][j]}'
+                elif (i, j) in self.flags:
+                    row += ' F'  # If the cell has a flag, add the flag symbol
                 else:
-                    visible_board[row][col] = ' '
-        # put this together in a string
-        string_rep = ''
-        # put this together in a string
-        widths = []
-        for idx in range(self.dim_size):
-            columns = [col for col in map(lambda x: x[idx], visible_board)]
-            widths.append(len(max(columns, key=len)))
-        # print the csv strings
-        indicies = [i for i in range(self.dim_size)]
-        indices_row = '   '
-        cells = []
-        for idx, col in enumerate(indicies):
-            format = '%-' + str(widths[idx]) + 's'
-            cells.append(format % (col))
-        indices_row += '  '.join(cells)
-        indices_row += '  \n'
+                    row += ' -'  # Otherwise, add the covered cell symbol
+            s += f'{row}\n'
 
-        for i in range(len(visible_board)):
-            row = visible_board[i]
-            string_rep += f'{i} |'
-            cells = []
-            for idx, col in enumerate(row):
-                format = '%-' + str(widths[idx]) + 's'
-                cells.append(format % (col))
-            string_rep += ' |'.join(cells)
-            string_rep += ' |\n'
-
-        str_len = int(len(string_rep) / self.dim_size)
-        string_rep = indices_row + '-' * str_len + '\n' + string_rep + '-' * str_len
-
-        return string_rep
+        return s
 
 
-# play the game
 def play(dim_size=10, num_bombs=10):
-    # Step 1: create the board and plant the bombs
-    board = Board(dim_size, num_bombs)
+    """
+    Plays a game of Minesweeper.
 
-    # Step 2: show the user the board and ask them where they want to dig
-    # Step 3a: if location is a bomb, show game over message
-    # Step 3b: if location is not a bomb, dig recursively until each square is at
-    #   least next to a bomb
-    # Step 4: repeat steps 2 and 3 until there are no more places to dig -> VICOTRY!
+    Args:
+        dim_size (int): The size of the board.
+        num_bombs (int): The number of bombs on the board.
+    """
+    board = Board(dim_size, num_bombs)
+    board.assign_values_to_board()  # Populate board with numerical values
     safe = True
 
-    while len(board.dug) < board.dim_size**2 - board.num_bombs:
+    while len(board.dug) < board.dim_size ** 2 - board.num_bombs:
+        # print board
         print(board)
-        # 0,0 or 0, 0 or 0,    0
-        user_input = re.split(
-            ',(\\s)*',
-            input("Where would you like to dig? Input as row,col: "))
-        row, col = int(user_input[0]), int(user_input[-1])
-        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
-            print("Invalid location. Try again.")
-            continue
+        # get user input
+        user_input = input(
+            "Enter the coordinates for where you'd like to dig, or enter 'F' before the coordinates to add a flag: "
+        ).lower().replace(" ", "")
+        # handle flag input
+        if user_input.startswith("f"):
+            user_input = user_input[1:]
+            try:
+                row, col = map(int, user_input.split(","))
+                if not 0 <= row < board.dim_size or not 0 <= col < board.dim_size:
+                    raise ValueError
+            except ValueError:
+                print("Invalid flag location. Please try again.")
+                continue
+            if (row, col) in board.dug:
+                print("Cannot add flag to location that has already been dug.")
+                continue
+            board.flag(row, col)
+        # handle dig input
+        else:
+            try:
+                row, col = map(int, user_input.split(","))
+                if not 0 <= row < board.dim_size or not 0 <= col < board.dim_size:
+                    raise ValueError
+            except ValueError:
+                print("Invalid location. Please enter the row and column separated by a comma (e.g. 3,4).")
+                continue
+            if (row, col) in board.dug:
+                print("Location has already been dug. Please try again.")
+                continue
+            safe = board.dig(row, col)
+            if not safe:
+                break
 
-        # if it's valid, we dig
-        safe = board.dig(row, col)
-        if not safe:
-            # dug a bomb ahhhhhhh
-            break  # (game over rip)
+    # print final board
+    print(board)
 
-    # 2 ways to end loop, lets check which one
+    # check game outcome
     if safe:
-        print("CONGRATULATIONS!!!! YOU ARE VICTORIOUS!")
+        print("Congratulations! You won!")
     else:
-        print("SORRY GAME OVER:")
-        #let's reveal the whole board!
-        board.dug = [(r, c) for r in range(board.dim_size)
-                     for c in range(board.dim_size)]
-        print(board)
+        print("Sorry, you hit a bomb. Game over!")
 
-
-if __name__ == '__main__':  # good practice
+if __name__ == '__main__':
     play()
